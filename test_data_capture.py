29988a0ed6a0532cb273cbaeaa0eeacc844c5774
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-01-19 11:11:02 -0500

    refactor tests

--- test_data_capture.py
+++ test_data_capture.py
@@ -5,26 +5,23 @@ from data_capture import DataCapture
 
 class TestDataCapture(unittest.TestCase):
     def setUp(self) -> None:
-        numbers = [3, 9, 3, 4, 6, 5]
-        self.data_capture = DataCapture(numbers)
-
-    def test_empty_init(self):
-        data_capture = DataCapture()
-        data_capture.add(5)
-        data_capture.add(6)
-        data_capture.add(3)
-        data_capture.add(4)
-        data_capture.add(9)
+        self.data_capture = DataCapture()
+        self.data_capture.add(3)
+        self.data_capture.add(9)
+        self.data_capture.add(3)
+        self.data_capture.add(4)
+        self.data_capture.add(6)
+        self.data_capture.add(5)
 
     def test_positive_numbers(self):
         self.data_capture.add(10)
-        self.assertEqual(self.data_capture.numbers.size, 7)
+        self.assertEqual(len(self.data_capture.numbers), 7)
 
     def test_negative_numbers(self):
         with self.assertRaises(ValueError):
             self.data_capture.add(-203)
 
-    def test_exceptiom_stats(self):
+    def test_exception_stats(self):
         with self.assertRaises(AttributeError):
             self.data_capture.greater(8)
             self.data_capture.less(7)
@@ -35,6 +32,13 @@ class TestDataCapture(unittest.TestCase):
         self.assertEqual(self.data_capture.greater(2), 6)
         self.assertEqual(self.data_capture.less(7), 5)
         self.assertEqual(self.data_capture.between(2, 6), 5)
+
+    def test_invalid_stats(self):
+        self.data_capture.build_stats()
+        with self.assertRaises(ValueError):
+            self.data_capture.greater(-2)
+            self.data_capture.less(1001)
+            self.data_capture.between(-4, 2032)
 
 
 if __name__ == "__main__":
