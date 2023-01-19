96db69a7fc880d36fec8a38083e75d6981a5af74
Juan Carlos Espinoza Zurita <carlos_jcez@hotmail.com>
2023-01-19 11:10:51 -0500

    refactor build_stats

--- data_capture.py
+++ data_capture.py
@@ -2,11 +2,9 @@ import numpy as np
 
 
 class DataCapture:
-    def __init__(self, numbers: list[int] = []) -> None:
-        numbers = np.array(numbers)
-        if numbers[numbers < 0].size > 0:
-            raise ValueError("Only positive numbers allowed")
-        self.numbers = numbers
+    def __init__(self) -> None:
+        self.numbers = []
+        self.stats = {}
 
     def validate_element(self, number: int) -> None:
         if number < 0 or number > 1000:
@@ -14,31 +12,31 @@ class DataCapture:
 
     def add(self, number: int) -> None:
         self.validate_element(number)
-        self.numbers = np.append(self.numbers, number)
+        self.numbers.append(number)
 
     def build_stats(self) -> None:
-        numbers = range(1, 1001)
-        self.greater_numbers = {
-            num: self.numbers[self.numbers > num].size for num in numbers
-        }
-        self.less_numbers = {
-            num: self.numbers[self.numbers < num].size for num in numbers
+        numbers = range(0, 1001)
+        np_numbers = np.array(self.numbers)
+        self.stats = {
+            num: (np_numbers[np_numbers > num].size, np_numbers[np_numbers < num].size)
+            for num in numbers
         }
 
     def greater(self, number: int) -> int:
         self.validate_element(number)
-        if not hasattr(self, "greater_numbers"):
+        if not self.stats:
             raise AttributeError("Please execute build_stats")
-        return self.greater_numbers[number]
+        return self.stats[number][0]
 
     def less(self, number: int) -> int:
         self.validate_element(number)
-        if not hasattr(self, "less_numbers"):
+        if not self.stats:
             raise AttributeError("Please execute build_stats")
-        return self.less_numbers[number]
+        return self.stats[number][1]
 
     def between(self, start_number: int, end_number: int) -> int:
         self.validate_element(start_number)
-        if not hasattr(self, "greater_numbers") or not hasattr(self, "less_numbers"):
+        self.validate_element(end_number)
+        if not self.stats:
             raise AttributeError("Please execute build_stats")
-        return self.numbers.size - self.greater(end_number) - self.less(start_number)
+        return len(self.numbers) - self.greater(end_number) - self.less(start_number)
